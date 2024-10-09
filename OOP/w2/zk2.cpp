4bdf9452981d7ffffd5302b8e90294426d46d1f9
WojciechIgnaczak <wojciech.ignaczak@onet.pl>
2024-10-09 19:47:45 +0200

    zk2

---
+++ OOP/w2/zk2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Complex{
+    public:
+        Complex(double _re,double _im): re(_re), im(_im){};
+        Complex(double _re):re(_re),im(0){};
+
+    double real()const{return re;};
+    double imag()const{return im;};
+    Complex operator+(const Complex &c);
+    Complex operator-(const Complex &c);
+    Complex& operator+=(const Complex &c);
+    Complex& operator-=(const Complex &c);
+    Complex operator-(); //unarny jednoargumentowy
+    Complex operator*(double x);
+
+    private:
+        double re,im;
+};
+
+
+Complex Complex::operator+(const Complex &c){
+    return Complex(re+c.re,im+c.im);
+}
+
+Complex Complex::operator-(const Complex &c){
+    return Complex(re-c.re,im-c.im);
+}
+
+Complex &Complex::operator+=(const Complex &c){
+    re+=c.re;
+    im+=c.im;
+    return *this;
+}
+
+Complex &Complex::operator-=(const Complex &c){
+    re-=c.re;
+    im-=c.im;
+    return *this; 
+}
+
+Complex Complex::operator-(){
+    return Complex(-re,-im);
+}
+
+Complex Complex::operator*(double x){
+    return Complex(re*x,im*x);
+}
+ostream& operator<<(ostream &os, const Complex &obj) {
+    if (obj.imag()>0)
+    {
+        os << obj.real() << " + " << obj.imag() << "i";
+    }
+    else
+    {
+        os << obj.real()<< obj.imag() << "i";
+    }
+    return os;
+};
+
+int main()
+{
+Complex c1(1,1);
+Complex c2(2,2);
+cout<<c2*3<<endl;
+return 0;
+}
