8cde63a79de179d49b74e93a849cac31a1ecd5fb
WojciechIgnaczak <wojciech.ignaczak@onet.pl>
2025-03-07 11:03:34 +0000

    s

---
+++ MOBILE_APP/wz2/w.md
@@ -0,0 +1,17 @@
+# paradygmaty programowania obiektowegi
+- dziedziczenie
+- polimorfizm
+- enkapsulacja
+- abstrakcja (bez dziedziczenia tak naprawdę nie ma)
+
+Jest jeszcze jedna koncepcja, którą należy rozważyć w połączeniu z trzema powyższymi: *kompozycja*.
+
+nie ma zmiennych globalnych
+
+
+
+
+
+##
+znajdz przykład i opisz obiekt świata rzeczywistego w programowaniu obiektowym
+(nie samochody nie kształty)
---
+++ OOP2/wz2/oopy.py
@@ -0,0 +1,95 @@
+# składający wiersze tekstu
+# wiesz posiada swój numer
+# identyfikator wiersza jako napis
+# identyfikator nieunikalny
+
+
+
+# klasy abstrakcyjne
+class TextLines():
+    def __init__(self):
+        pass
+    
+    def readLine(self,numerWiersza): # wiersz o zadanym numerze
+        pass
+
+    def writeLine(self,text): # dodanie wiersza na końcu
+       pass
+    
+    def writeLine(self,text,numerWiersza):# dodanie wiersza w konkretnej pozycji, nie pozwalamy na dodanie wiersza z przerwą, gdy linia istnieje to zostaje nadpisany
+       pass
+
+    def deleteLine(self,numerWiersza): # usuwanie wiersza i przenumerowanie tych poniżej
+        pass
+    
+    def getNumberOfLines():
+        pass
+
+class IdentifiedTextLines(TextLines): 
+    def __init__(self):
+        pass
+
+    def readLine(self,key): # wiersze! o zadanym numerze
+        pass
+
+    def writeLine(self,key,text): # dodanie wiersza na końcu
+       pass
+    
+    def writeLine(self,key,text,numerWiersza):# dodanie wiersza w konkretnej pozycji, nie pozwalamy na dodanie wiersza z przerwą, gdy linia istnieje to zostaje nadpisany
+       pass
+
+    def deleteLine(self,identyfikator): # usuwanie wiersza i przenumerowanie tych poniżej
+        pass
+
+class UniquelyIdentifiedTextLines():
+    IdentifiedTextLines
+
+
+# Klasy "realne"
+
+class TextLinesInMemory(TextLines):
+    def __init__(self):
+        pass
+
+class TextLinesInFile(TextLines):
+    def __init__(self):
+        pass
+
+class TextLinesInDB(TextLines):
+    def __init__(self):
+        pass
+
+
+
+
+class IdentifiedTextLinesInMemory(IdentifiedTextLines):
+    def __init__(self):
+        pass
+
+class IdentifiedTextLinesInFIle(IdentifiedTextLines):
+    def __init__(self):
+        pass
+
+class IdentifiedTextLinesInDB(IdentifiedTextLines):
+    def __init__(self):
+        pass
+    
+
+
+class UniquelyIdentifiedTextLinesInMemory(UniquelyIdentifiedTextLines):
+    def __init__(self):
+        pass
+
+class UniquelyIdentifiedTextLinesInFIle(UniquelyIdentifiedTextLines):
+    def __init__(self):
+        pass
+
+class UniquelyIdentifiedTextLinesInDB(UniquelyIdentifiedTextLines):
+    def __init__(self):
+        pass
+    
+
+def main():
+    pass
+if __name__ =='__main__':
+    main()
